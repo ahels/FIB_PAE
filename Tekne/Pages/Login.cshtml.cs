7109337da356ce70c66406cef526d077e9582b5a
Alfonso Hels <110061538+ahels@users.noreply.github.com>
2024-10-12 13:11:03 +0200

    Initial Commit

---
+++ Tekne/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tekne.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnPost()
+        {
+        }
+    }
+}
