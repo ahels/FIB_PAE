9962e59ba9e1a7653f9f1a6f51441105f7b3cb4e
aantunezc <100408523+aantunezc@users.noreply.github.com>
2025-01-06 21:37:39 +0100

    solucion problemas de cajas

--- Tekne/wwwroot/js/simulator.js
+++ Tekne/wwwroot/js/simulator.js
@@ -76,12 +76,6 @@ function animate() {
 animate();
 
 
-const debugSphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.1),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
-
-//scene.add(debugSphere);
 
 
 /* Seleccionar elemento */
@@ -131,31 +125,34 @@ function loadModel(modelPath) {
     loader.load(modelPath, (gltf) => {
         const model = gltf.scene;
         model.name = modelPath.split('.')[0];
+        model.name = model.name.split('/')[2];
         model.position.set(0, 0, 0);
         model.scale.set(0.2, 0.2, 0.2);
 
 
 
-        if (model.name === "box test") { // Comprova si el model és box.glb
+        /*if (model.name === "box_test") { // Comprova si el model és box.glb
             model.traverse((child) => {
                 if (child.isMesh) {
                     child.material.depthTest = false; // Ignora profunditat
                     child.material.depthWrite = false; // No escriu a la profunditat
                 }
             });
-        }
+        }*/
 
 
         const box = new THREE.Box3().setFromObject(model); // Caixa d'encapsulament del model
+        const size = box.getSize(new THREE.Vector3()); // Obtenim les dimensions del model
         const center = box.getCenter(new THREE.Vector3()); // Centre del model
         model.position.sub(center); // Ajustar la posició perquè el pivot quedi al centre
 
         const pivot = new THREE.Object3D(); // Crear el pivot
         pivot.add(model); // Afegir el model al pivot
-        pivot.name = modelPath.split('.')[0];
+        pivot.name = model.name;
         pivot.position.copy(center); // Posicionar el pivot on hauria d'estar el model
 
         pivot.orientacio = 0;
+        pivot.height = size.y; // Afegir la propietat d'alçada al pivot
 
         scene.add(pivot);
         models.push(model); // Afegim el pivot a la llista
@@ -170,9 +167,9 @@ function loadModel(modelPath) {
 canvas.addEventListener("mousemove", (event) => {
     const intersectionPoint = calculateMousePosition(event);
     if (intersectionPoint) {
-        debugSphere.position.copy(intersectionPoint); // Mou l'esfera
+        
         if (dragTarget) {
-            dragTarget.position.set(intersectionPoint.x, 0, intersectionPoint.z);
+            dragTarget.position.set(intersectionPoint.x, (dragTarget.height/2), intersectionPoint.z);
 
         }
     }
@@ -228,7 +225,7 @@ function checkCollision(targetPivot) {
     const targetBox = new THREE.Box3().setFromObject(targetPivot); // Caixa que envolta el pivot
 
     for (const pivot of pivots) {
-        if (pivot !== targetPivot && pivot.name !== "box test") { // No comparar amb ell mateix
+        if (pivot !== targetPivot && pivot.name !== "box_test") { // No comparar amb ell mateix
             const pivotBox = new THREE.Box3().setFromObject(pivot);
             if (targetBox.intersectsBox(pivotBox)) { // Comprova col·lisió
                 return pivot.orientacio; // Col·lisió detectada
@@ -241,7 +238,7 @@ function checkCollision(targetPivot) {
 
 function moveCollidingPivots(speed) {
     pivots.forEach((pivot) => {
-        if (pivot.name === "box test") {
+        if (pivot.name === "box_test") {
             orientacio = checkCollision(pivot);
             switch (orientacio) {
                 case -1:
